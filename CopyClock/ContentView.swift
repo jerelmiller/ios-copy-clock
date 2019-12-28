ef613a8d27990547e69ac6545d3e823c935181ae
Jerel Miller <jerelmiller@gmail.com>
2019-12-28 00:07:13 -0800

    Initial Commit

---
+++ CopyClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CopyClock
+//
+//  Created by Jerel Miller on 12/28/19.
+//  Copyright © 2019 Jerel Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
